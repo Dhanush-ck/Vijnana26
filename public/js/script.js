84b5ce0347bba428b4841bdd470f259ea82d66eb
Dhanush CK <dhanushck01@gmail.com>
2025-12-09 23:43:12 +0530

    feat: add js file

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const day = document.getElementById('day');
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+
+day.innerHTML = hour.innerHTML = minute.innerHTML = second.innerHTML = "00";
